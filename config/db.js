989a72bbc714cd027ac08d1c4f79b461d2c34e1d
Davidtanri <davidtanri1994@gmail.com>
2022-06-07 19:07:23 +0700

    origin

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const sequelize = require("sequelize");
 
-const db = new sequelize("db_kalpataru", "root", "", {
+const db = new sequelize("db_kalpataru", "root", "kalpataru", {
+    host: '34.101.253.45',
     dialect: "mysql"
 });
 
